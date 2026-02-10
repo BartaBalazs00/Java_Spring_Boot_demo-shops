91de0b055db54f23f2eb43663dcdded21606af46
Barta Balazs <barta.balazs00@gmail.com>
2026-02-10 17:19:20 +0100

    Initial with entities

---
+++ src/main/java/com/demoproject/demoshops/DemoShopsApplication.java
@@ -0,0 +1,13 @@
+package com.demoproject.demoshops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoShopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoShopsApplication.class, args);
+	}
+
+}
