fa4f6c89c1f0ffca1cf3936a2f27a2696c710070
Barta Balazs <barta.balazs00@gmail.com>
2026-02-11 17:08:35 +0100

    IProductService added

---
+++ src/main/java/com/demoproject/demoshops/service/products/IProductService.java
@@ -0,0 +1,21 @@
+package com.demoproject.demoshops.service.products;
+
+import com.demoproject.demoshops.model.Product;
+
+import java.util.List;
+
+public interface IProductService {
+    Product addProduct(Product product);
+    Product getProductById(Long id);
+    void deleteProductById(Long id);
+    void updateProduct(Product product, Long productId);
+    List<Product> getAllProducts();
+    List<Product> getProductsByCategory(String category);
+    List<Product> getProductsByBrand(String brand);
+    List<Product> getProductsByCategoryAndBrands(String category, String brand);
+    List<Product> getProductsByName(String name);
+    List<Product> getProductsByBrandAndName(String brand, String name);
+    Long countProductsByBrandAndName(String brand, String name);
+
+
+}
