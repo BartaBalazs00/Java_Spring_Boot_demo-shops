32088ffad52f5a7d97896eb49ab6d864aae7fa44
Barta Balazs <barta.balazs00@gmail.com>
2026-02-11 17:12:34 +0100

    ProductService added

---
+++ src/main/java/com/demoproject/demoshops/service/products/ProductService.java
@@ -0,0 +1,64 @@
+package com.demoproject.demoshops.service.products;
+
+import com.demoproject.demoshops.model.Product;
+import com.demoproject.demoshops.repository.ProductRepository;
+
+import java.util.List;
+
+public class ProductService implements IProductService {
+    private ProductRepository productRepository;
+    @Override
+    public Product addProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteProductById(Long id) {
+
+    }
+
+    @Override
+    public void updateProduct(Product product, Long productId) {
+
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByCategory(String category) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByBrand(String brand) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByCategoryAndBrands(String category, String brand) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByBrandAndName(String brand, String name) {
+        return List.of();
+    }
+
+    @Override
+    public Long countProductsByBrandAndName(String brand, String name) {
+        return 0L;
+    }
+}
