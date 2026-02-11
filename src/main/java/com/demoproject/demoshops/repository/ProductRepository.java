e5a3c218b2f9aecebb59d94053126cde749ea816
Barta Balazs <barta.balazs00@gmail.com>
2026-02-11 17:26:42 +0100

    Productrepository added

---
+++ src/main/java/com/demoproject/demoshops/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.demoproject.demoshops.repository;
+
+import com.demoproject.demoshops.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
