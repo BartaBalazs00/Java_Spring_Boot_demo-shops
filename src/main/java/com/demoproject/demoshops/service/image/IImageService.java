c07259a590e8abc84a1353700e817eb60927cdfb
Barta Balazs <barta.balazs00@gmail.com>
2026-02-17 19:58:43 +0100

    ImageService implemented

--- src/main/java/com/demoproject/demoshops/service/image/IImageService.java
+++ src/main/java/com/demoproject/demoshops/service/image/IImageService.java
@@ -1,4 +1,14 @@
 package com.demoproject.demoshops.service.image;
 
+import com.demoproject.demoshops.dto.ImageDto;
+import com.demoproject.demoshops.model.Image;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 public interface IImageService {
+    Image getImageById(Long id);
+    void deleteImageById(Long id);
+    List<ImageDto> saveImage(List<MultipartFile> files, Long productId);
+    void updateImage(MultipartFile file, Long imageId);
 }
